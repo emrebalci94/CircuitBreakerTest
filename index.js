c1207585eb9aa16aa24d74fee28929996405543f
Emre Balcı <emrebalci94@hotmail.com>
2019-01-23 09:30:33 +0300

    NO-ISSUE / Retry Api call added.

--- index.js
+++ index.js
@@ -3,36 +3,39 @@ var app = express();
 var opossum = require("opossum");
 var axios = require("axios");
 
-function asyncFunctionThatCouldFail() {
+function asyncFunctionThatCouldFail(count = 3, timeOut = 3000) {
   return new Promise((resolve, reject) => {
-    // Do something, maybe on the network or a disk
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos/1/2")
-      .then(p => resolve(p))
-      .catch(e => reject(e));
+    function run() {
+      axios.get("https://jsonplaceholder.typicode.com/todos/1/2").then(
+        p => {
+          console.log("p", p.data);
+          resolve(p.data);
+        },
+        p => {
+          if (count >= 1) {
+            console.log(count, " deneme kaldı.");
+            count--;
+            setTimeout(run, timeOut);
+          } else {
+            reject(new Error(p));
+          }
+        }
+      );
+    }
+    run();
   });
 }
 
-const options = {
-  timeout: 10000, // If our function takes longer than 3 seconds, trigger a failure
-  errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
-  resetTimeout: 30000 // After 30 seconds, try again.
-};
-const breaker = opossum(asyncFunctionThatCouldFail, options);
-
-app.route("/test").get((req, res, next) => {
-  breaker
-    .fire()
-    .then(res.send("basıntı"))
-    .catch(e => console.log(e.message));
+app.route("/test2").get(async (req, res, next) => {
+  try {
+    const data = await asyncFunctionThatCouldFail(2, 500);
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    console.log("Hata:", error.stack);
+    res.send(404, error.message);
+  }
 });
 
-breaker.fallback(() => "Sorry, out of service right now");
-breaker.on("fallback", result => console.log(result));
-breaker.on("success", result => {
-  // res.send(result);
-  //   result.send("test");
-  console.log("result");
-});
 app.listen(1453);
 console.log(`Server running at localhost:${1453}`);
